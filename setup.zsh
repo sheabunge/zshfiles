f92bac8598763c89a4647799cabf02aaf9e9d6d8
Shea Bunge <info@bungeshea.com>
2016-03-03 19:49:52 +1100

    Add setup script

---
+++ setup.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/zsh
+
+# Load plugins
+plugins=(
+	git github git-flow git-extras
+	composer npm vagrant
+	common-aliases command-not-found
+	zsh-completions zsh-syntax-highlighting
+)
+
+
+# Load theme
+ZSH_THEME="gallifrey"
+
+# Set the command execution time stamp shown in the history command output.
+# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+HIST_STAMPS="dd.mm.yyyy"
+
+export SSH_KEY_PATH="$HOME/.ssh/id_rsa"
+
+
+
