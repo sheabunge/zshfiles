50c1b629ae5364ff65c8e6c8ecb674bfd0aadc0e
Shea Bunge <info@bungeshea.com>
2016-03-06 18:51:05 +1100

    Add disabled settings from .zshrc

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-# Load plugins
+# Load plugins - loading too many will slow down the shell
 plugins=(
 	git github git-flow git-extras
 	composer npm vagrant
@@ -12,10 +12,34 @@ plugins=(
 # Load theme
 ZSH_THEME="gallifrey"
 
+# Enable case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Use hyphen-insensitive completion. Case sensitive completion must be off.
+# _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Display nred dots whilst waiting for completion.
+# COMPLETION_WAITING_DOTS="true"
+
+# Disable marking untracked files under VCS as dirty. This makes repository
+# status check for large repositories much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
 # Set the command execution time stamp shown in the history command output.
 # The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
 HIST_STAMPS="dd.mm.yyyy"
 
+# ssh
 export SSH_KEY_PATH="$HOME/.ssh/id_rsa"
 
 
